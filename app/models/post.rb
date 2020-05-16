072af03279d85d2bbe864312e4b7266b32b5bab6
Joseph Hung <arixxle@gmail.com>
2020-05-16 12:50:31 +0800

    set up model post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,15 @@
 class Post < ApplicationRecord
   belongs_to :user
+  #Active Storage form Rails 5
+  has_one_attachied :thumbmail
+  has_one_attachied :banner
+  #Action Text from Rails 6
+  has_rich_text :body
+
+  validates :title, length: ( minimum: 5 )
+  validates :body,  length: ( minimum: 25 )
+
+  def optimized_image(image, x, y)
+    return image.variant(resize_to_fill: [x, y]).processed
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :posts
   devise_for :users
+  root 'posts#index'
   get 'about', to: 'pages#about'
-  root 'pages#about'
 end
