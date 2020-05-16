cb14d1c2a3a25cd90d7ab4759ff0672997c05c54
Joseph Hung <arixxle@gmail.com>
2020-05-16 13:08:22 +0800

    fix some syntax error

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,13 @@
 class Post < ApplicationRecord
   belongs_to :user
   #Active Storage form Rails 5
-  has_one_attachied :thumbmail
-  has_one_attachied :banner
+  has_one_attached :thumbnail
+  has_one_attached :banner
   #Action Text from Rails 6
   has_rich_text :body
 
-  validates :title, length: ( minimum: 5 )
-  validates :body,  length: ( minimum: 25 )
+  validates :title, length: { minimum: 5 }
+  validates :body,  length: { minimum: 25 }
 
   def optimized_image(image, x, y)
     return image.variant(resize_to_fill: [x, y]).processed
