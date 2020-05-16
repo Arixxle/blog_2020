0ceab4155bc1452ca501f4ed924472837076f551
Joseph Hung <arixxle@gmail.com>
2020-05-16 12:04:48 +0800

    add a username method to split the name from email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  # joseph@example.com -> self.email.split('@') -> ["joseph", "example.com"] -> [0] ->"joseph".capitalize -> "Joseph"
+  def username
+    return email.split('@')[0].capitalize
+  end
 end
