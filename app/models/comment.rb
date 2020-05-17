6ca4b7f5fa609772b82cfb01835d9ffa63196111
Joseph Hung <arixxle@gmail.com>
2020-05-17 13:00:30 +0800

    issue #4 set up Trix to comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :user
-
   before_create :set_comment_number
+  has_rich_text :body
 
+  private
   def set_comment_number
     self.comment_number = user.comment_created
   end
