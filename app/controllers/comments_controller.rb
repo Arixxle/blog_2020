179b608e00f2b3e97c8498c9b107092c423696be
Joseph Hung <arixxle@gmail.com>
2020-05-17 13:13:04 +0800

    issue #1 add comments controller and set up

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,26 @@
+class CommentsController < ApplicationController
+
+  def create
+    
+  end
+  def edit
+    
+  end
+  def destroy
+    
+  end
+  def history
+    
+  end
+
+  private
+  def comment_params
+    
+  end
+  def find_commentable
+    
+  end
+  def set_comment
+    
+  end
+end
