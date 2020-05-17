b8ce7a2ba3804e89f352cf6297348002bd43543d
Joseph Hung <arixxle@gmail.com>
2020-05-17 13:28:00 +0800

    issue #1 set up comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,13 @@
 class CommentsController < ApplicationController
+  before_action :find_commentable, only: [:create]
+  before_action :set_comment, except: [:create]
+  before_action :authenticate_user!
 
   def create
-    
+    @comment = @commetable.comment.build(comment_params)
+    @comment.user = current_user
+    @comment.reply = true if params[:comment_id]
+    @comment.save
   end
   def edit
     
@@ -15,12 +21,18 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    
+    params.require(:comment).permit(:body)
   end
   def find_commentable
-    
+    #Comment
+    if params[:comment_id]
+      @commentable = Comment.find_by_id(params[:comment_id])
+    elsif params[:post_id]
+    #Post
+      @commentable = Post.friendly.find(params[:post_id])
+    end
   end
   def set_comment
-    
+    @comment = Comment.find(params[:id])
   end
 end
