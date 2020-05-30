d226e9f920faf1178ebe0acedaa88bf7b8898a1f
Joseph Hung <arixxle@gmail.com>
2020-05-30 11:28:43 +0800

    add comments/Modal

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :comments, dependent: :destroy
+  has_one_attached :avatar
   # joseph@example.com -> self.email.split('@') -> ["joseph", "example.com"] -> [0] ->"joseph".capitalize -> "Joseph"
   def username
     return email.split('@')[0].capitalize
